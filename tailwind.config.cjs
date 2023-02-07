b25f82cb8bae64ec6408829e36584dc77ba0e51e
devga <biennguyen131311@gmail.com>
2023-02-08 02:07:13 +0700

    #152

---
+++ tailwind.config.cjs
@@ -0,0 +1,12 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable @typescript-eslint/no-var-requires */
+const plugin = require('tailwindcss/plugin')
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {}
+  },
+  plugins: []
+}
