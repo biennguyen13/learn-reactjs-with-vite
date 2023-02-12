0b51a57ecb039989eb1c19c257ef0af52f0bb15f
devga <biennguyen131311@gmail.com>
2023-02-12 17:52:30 +0700

    #155

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,3 +1,55 @@
+import { useForm } from 'react-hook-form'
+import { Link } from 'react-router-dom'
+import Button from 'src/components/Button'
+import Input from 'src/components/Input'
+
 export default function Login() {
-  return <div>Login page</div>
+  const {
+    register,
+    handleSubmit,
+    formState: { errors }
+  } = useForm()
+
+  const onSubmit = handleSubmit((data) => {})
+
+  return (
+    <div className='bg-orange'>
+      {/* <Helmet>
+        <title>Đăng nhập | Shopee Clone</title>
+        <meta name='description' content='Đăng nhập vào dự án Shopee Clone' />
+      </Helmet> */}
+      <div className='container'>
+        <div className='grid grid-cols-1 py-12 lg:grid-cols-5 lg:py-32 lg:pr-10'>
+          <div className='lg:col-span-2 lg:col-start-4'>
+            <form className='bg-white rounded p-10 shadow-sm' noValidate onSubmit={onSubmit}>
+              <div className='text-2xl'>Đăng nhập</div>
+              <Input name='email' type='email' className='mt-8' placeholder='Email' />
+              <Input
+                name='password'
+                type='password'
+                className='mt-2'
+                classNameEye='absolute right-[5px] h-5 w-5 cursor-pointer top-[12px]'
+                placeholder='Password'
+                autoComplete='on'
+              />
+              <div className='mt-3'>
+                <Button
+                  type='submit'
+                  className='bg-red-500  text-white hover:bg-red-600 flex w-full items-center justify-center py-4 px-2 text-sm uppercase'
+                >
+                  Đăng nhập
+                </Button>
+              </div>
+              <div className='mt-8 flex items-center justify-center'>
+                <span className='text-gray-400'>Bạn chưa có tài khoản?</span>
+                <Link className='text-red-400 ml-1' to='/register'>
+                  Đăng ký
+                </Link>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 }
--- src/pages/Register/Register.tsx
+++ src/pages/Register/Register.tsx
@@ -1,3 +1,62 @@
+import { useForm } from 'react-hook-form'
+import { Link } from 'react-router-dom'
+import Button from 'src/components/Button'
+import Input from 'src/components/Input'
+
 export default function Register() {
-  return <div>Register page</div>
+  const {
+    register,
+    handleSubmit,
+    formState: { errors }
+  } = useForm()
+
+  const onSubmit = handleSubmit((data) => {
+    console.log(data)
+  })
+
+  return (
+    <div className='bg-orange'>
+      {/* <Helmet>
+        <title>Đăng ký | Shopee Clone</title>
+        <meta name='description' content='Đăng ký tài khoản vào dự án Shopee Clone' />
+      </Helmet> */}
+      <div className='container'>
+        <div className='grid grid-cols-1 py-12 lg:grid-cols-5 lg:py-32 lg:pr-10'>
+          <div className='lg:col-span-2 lg:col-start-4'>
+            <form className='bg-white rounded p-10 shadow-sm' noValidate onSubmit={onSubmit}>
+              <div className='text-2xl'>Đăng ký</div>
+              <Input type='email' className='mt-8' placeholder='Email' />
+              <Input
+                type='password'
+                className='mt-2'
+                classNameEye='absolute right-[5px] h-5 w-5 cursor-pointer top-[12px]'
+                placeholder='Password'
+                autoComplete='on'
+              />
+
+              <Input
+                type='password'
+                className='mt-2'
+                classNameEye='absolute right-[5px] h-5 w-5 cursor-pointer top-[12px]'
+                placeholder='Confirm Password'
+                autoComplete='on'
+              />
+
+              <div className='mt-2'>
+                <Button className='bg-red-500 text-white hover:bg-red-600 flex w-full items-center justify-center py-4 px-2 text-sm uppercase'>
+                  Đăng ký
+                </Button>
+              </div>
+              <div className='mt-8 flex items-center justify-center'>
+                <span className='text-gray-400'>Bạn đã có tài khoản?</span>
+                <Link className='text-red-400 ml-1' to='/login'>
+                  Đăng nhập
+                </Link>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 }
