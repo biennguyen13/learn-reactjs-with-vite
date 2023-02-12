d9a14ad21994625e8ae9959dffd8954f6d2abe01
devga <biennguyen131311@gmail.com>
2023-02-13 01:02:44 +0700

    #158

--- src/pages/Register/Register.tsx
+++ src/pages/Register/Register.tsx
@@ -33,48 +33,48 @@ export default function Register() {
 
   return (
     <div className='bg-orange'>
-      <div className='mx-auto max-w-7xl px-4'>
-        <div className='grid grid-cols-1 py-12 lg:grid-cols-5 lg:py-32 lg:pr-10'>
+      <div className='container'>
+        <div className='grid grid-cols-1 lg:grid-cols-5 py-12 lg:py-32 lg:pr-10'>
           <div className='lg:col-span-2 lg:col-start-4'>
-            <form className='bg-white rounded p-10 shadow-sm' onSubmit={onSubmit} noValidate>
+            <form className='p-10 rounded bg-white shadow-sm' onSubmit={onSubmit} noValidate>
               <div className='text-2xl'>Đăng ký</div>
               <div className='mt-8'>
                 <input
                   type='email'
-                  className='border-gray-300 focus:border-gray-500 w-full rounded-sm border p-3 outline-none focus:shadow-sm'
+                  className='p-3 w-full outline-none border border-gray-300 focus:border-gray-500 rounded-sm focus:shadow-sm'
                   placeholder='Email'
                   {...register('email', rules.email)}
                 />
-                <div className='text-red-600 mt-1 min-h-[1.25rem] text-sm'>{errors.email?.message}</div>
+                <div className='mt-1 text-red-600 min-h-[1.25rem] text-sm'>{errors.email?.message}</div>
               </div>
               <div className='mt-2'>
                 <input
                   type='password'
-                  className='border-gray-300 focus:border-gray-500 w-full rounded-sm border p-3 outline-none focus:shadow-sm'
+                  className='p-3 w-full outline-none border border-gray-300 focus:border-gray-500 rounded-sm focus:shadow-sm'
                   placeholder='Password'
                   autoComplete='on'
                   {...register('password', rules.password)}
                 />
-                <div className='text-red-600 mt-1 min-h-[1.25rem] text-sm'>{errors.password?.message}</div>
+                <div className='mt-1 text-red-600 min-h-[1.25rem] text-sm'>{errors.password?.message}</div>
               </div>
               <div className='mt-2'>
                 <input
                   type='password'
-                  className='border-gray-300 focus:border-gray-500 w-full rounded-sm border p-3 outline-none focus:shadow-sm'
+                  className='p-3 w-full outline-none border border-gray-300 focus:border-gray-500 rounded-sm focus:shadow-sm'
                   placeholder='Confirm Password'
                   autoComplete='on'
                   {...register('confirm_password', {
                     ...rules.confirm_password
                   })}
                 />
-                <div className='text-red-600 mt-1 min-h-[1.25rem] text-sm'>{errors.confirm_password?.message}</div>
+                <div className='mt-1 text-red-600 min-h-[1.25rem] text-sm'>{errors.confirm_password?.message}</div>
               </div>
               <div className='mt-2'>
-                <button className='bg-red-500 text-white hover:bg-red-600 w-full py-4 px-2 text-center text-sm uppercase'>
+                <button className='w-full text-center py-4 px-2 uppercase bg-red-500 text-white text-sm hover:bg-red-600'>
                   Đăng ký
                 </button>
               </div>
-              <div className='mt-8 flex items-center justify-center'>
+              <div className='flex items-center justify-center mt-8'>
                 <span className='text-gray-400'>Bạn đã có tài khoản?</span>
                 <Link className='text-red-400 ml-1' to='/login'>
                   Đăng nhập
