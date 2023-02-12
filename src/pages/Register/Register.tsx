a98538f5dbdbb3a48207f7d99b4f78a2144a866b
devga <biennguyen131311@gmail.com>
2023-02-12 23:55:22 +0700

    #157

--- src/pages/Register/Register.tsx
+++ src/pages/Register/Register.tsx
@@ -1,51 +1,78 @@
-import { useForm } from 'react-hook-form'
 import { Link } from 'react-router-dom'
-import Button from 'src/components/Button'
-import Input from 'src/components/Input'
+import { useForm } from 'react-hook-form'
+import { getRules } from 'src/utils/rules'
+
+interface FormData {
+  email: string
+  password: string
+  confirm_password: string
+}
 
 export default function Register() {
   const {
     register,
     handleSubmit,
+    watch,
+    getValues,
     formState: { errors }
-  } = useForm()
+  } = useForm<FormData>()
+  const rules = getRules(getValues)
+  const onSubmit = handleSubmit(
+    (data) => {
+      console.log(data)
+    },
+    (data) => {
+      const password = getValues('password')
 
-  const onSubmit = handleSubmit((data) => {
-    console.log(data)
-  })
+      console.log(password)
+    }
+  )
+
+  // const email = watch('password')
+  // console.log(email)
 
   return (
     <div className='bg-orange'>
-      {/* <Helmet>
-        <title>Đăng ký | Shopee Clone</title>
-        <meta name='description' content='Đăng ký tài khoản vào dự án Shopee Clone' />
-      </Helmet> */}
-      <div className='container'>
+      <div className='mx-auto max-w-7xl px-4'>
         <div className='grid grid-cols-1 py-12 lg:grid-cols-5 lg:py-32 lg:pr-10'>
           <div className='lg:col-span-2 lg:col-start-4'>
-            <form className='bg-white rounded p-10 shadow-sm' noValidate onSubmit={onSubmit}>
+            <form className='bg-white rounded p-10 shadow-sm' onSubmit={onSubmit} noValidate>
               <div className='text-2xl'>Đăng ký</div>
-              <Input type='email' className='mt-8' placeholder='Email' />
-              <Input
-                type='password'
-                className='mt-2'
-                classNameEye='absolute right-[5px] h-5 w-5 cursor-pointer top-[12px]'
-                placeholder='Password'
-                autoComplete='on'
-              />
-
-              <Input
-                type='password'
-                className='mt-2'
-                classNameEye='absolute right-[5px] h-5 w-5 cursor-pointer top-[12px]'
-                placeholder='Confirm Password'
-                autoComplete='on'
-              />
-
+              <div className='mt-8'>
+                <input
+                  type='email'
+                  className='border-gray-300 focus:border-gray-500 w-full rounded-sm border p-3 outline-none focus:shadow-sm'
+                  placeholder='Email'
+                  {...register('email', rules.email)}
+                />
+                <div className='text-red-600 mt-1 min-h-[1.25rem] text-sm'>{errors.email?.message}</div>
+              </div>
               <div className='mt-2'>
-                <Button className='bg-red-500 text-white hover:bg-red-600 flex w-full items-center justify-center py-4 px-2 text-sm uppercase'>
+                <input
+                  type='password'
+                  className='border-gray-300 focus:border-gray-500 w-full rounded-sm border p-3 outline-none focus:shadow-sm'
+                  placeholder='Password'
+                  autoComplete='on'
+                  {...register('password', rules.password)}
+                />
+                <div className='text-red-600 mt-1 min-h-[1.25rem] text-sm'>{errors.password?.message}</div>
+              </div>
+              <div className='mt-2'>
+                <input
+                  type='password'
+                  className='border-gray-300 focus:border-gray-500 w-full rounded-sm border p-3 outline-none focus:shadow-sm'
+                  placeholder='Confirm Password'
+                  autoComplete='on'
+                  {...register('confirm_password', {
+                    ...rules.confirm_password
+                  })}
+                />
+                <div className='text-red-600 mt-1 min-h-[1.25rem] text-sm'>{errors.confirm_password?.message}</div>
+              </div>
+              <div className='mt-2'>
+                <button className='bg-red-500 text-white hover:bg-red-600 w-full py-4 px-2 text-center text-sm uppercase'>
                   Đăng ký
-                </Button>
+                </button>
               </div>
               <div className='mt-8 flex items-center justify-center'>
                 <span className='text-gray-400'>Bạn đã có tài khoản?</span>
