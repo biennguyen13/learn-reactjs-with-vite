d87124c3918396060c59387e00ef81328a1364be
devga <biennguyen131311@gmail.com>
2023-02-22 00:17:36 +0700

    #201

--- src/components/QuantityController/QuantityController.tsx
+++ src/components/QuantityController/QuantityController.tsx
@@ -6,6 +6,7 @@ interface Props extends InputNumberProps {
   onIncrease?: (value: number) => void
   onDecrease?: (value: number) => void
   onType?: (value: number) => void
+  onFocusOut?: (value: number) => void
   classNameWrapper?: string
 }
 
@@ -14,6 +15,7 @@ export default function QuantityController({
   onIncrease,
   onDecrease,
   onType,
+  onFocusOut,
   classNameWrapper = 'ml-10',
   value,
   ...rest
@@ -34,6 +36,7 @@ export default function QuantityController({
     let _value = Number(value || localValue) + 1
     if (max !== undefined && _value > max) {
       _value = max
+      return
     }
     onIncrease && onIncrease(_value)
     setLocalValue(_value)
@@ -43,9 +46,14 @@ export default function QuantityController({
     let _value = Number(value || localValue) - 1
     if (_value < 1) {
       _value = 1
+      return
     }
     onDecrease && onDecrease(_value)
     setLocalValue(_value)
+  }
+
+  const handleBlur = (event: React.FocusEvent<HTMLInputElement, Element>) => {
+    onFocusOut && onFocusOut(Number(event.target.value))
   }
 
   return (
@@ -70,6 +78,7 @@ export default function QuantityController({
         classNameError='hidden'
         classNameInput='h-8 w-14 border-t border-b border-gray-300 p-1 text-center outline-none'
         onChange={handleChange}
+        onBlur={handleBlur}
         value={value || localValue}
         {...rest}
       />
