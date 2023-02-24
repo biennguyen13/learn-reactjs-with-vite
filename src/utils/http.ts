a1130fbfaef27a81a53b82544513262e99b9e56c
devga <biennguyen131311@gmail.com>
2023-02-25 02:48:09 +0700

    #224

--- src/utils/http.ts
+++ src/utils/http.ts
@@ -36,8 +36,8 @@ class Http {
       timeout: 10000,
       headers: {
         'Content-Type': 'application/json',
-        'expire-access-token': 300, // 10 giây
-        'expire-refresh-token': 3600 // 1 giờ
+        'expire-access-token': 60 * 60 * 24, // 1 ngày
+        'expire-refresh-token': 60 * 60 * 24 * 160 // 160 ngày
       }
     })
     this.instance.interceptors.request.use(
